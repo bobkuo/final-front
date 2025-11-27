fa39c3e373f169cef57a1e69c45ebf52b304ed67
bobkuo <geneteamgou@gmail.com>
2025-11-27 06:54:39 +0000

    Deploying to gh-pages from @ bobkuo/final-front@0d415c048e8664727334d17b06860c1f660c03e3 🚀

--- assets/order--uJNXXwZ.js
+++
@@ -1,1 +0,0 @@
-import{b8 as r}from"./index-DO5f-SfG.js";const t={create(){return r.apiAuth.post("/order")},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{t as o};
